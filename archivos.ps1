6a260c929eac04d86cdd5223e33710b6a8213091
Abraham-Carreon <62450261+Abraham-Carreon@users.noreply.github.com>
2023-11-08 17:09:41 -0600

    Script powershell

--- archivos.ps1
+++ archivos.ps1
@@ -1,8 +1,15 @@
 $path = "./investigacion" 
 
 If(!(test-path -PathType container $path)) { New-Item -ItemType Directory -Path $path }
+
 echo "Ya casi termina...."
-Move-Item -Path "./investigacionDominio.json" -Destination "./investigacion"
-Move-Item -Path "./metaimagenes.json" -Destination "./investigacion"
-Move-Item -Path "./metapdfs.json" -Destination "./investigacion"
-Move-Item -Path "./investigacionTecnologias.json" -Destination "./investigacion" 
+
+cd investigacion
+
+New-Item -ItemType HardLink -Path "./investigacionDominio.json" -Target "../investigacionDominio.json"
+New-Item -ItemType HardLink -Path "./metaimagenes.json" -Target "../metaimagenes.json"
+New-Item -ItemType HardLink -Path "./metapdfs.json" -Target "../metapdfs.json"
+New-Item -ItemType HardLink -Path "./investigacionTecnologias.json" -Target "../investigacionTecnologias.json"
+
+echo ""
+echo "Terminado!"
